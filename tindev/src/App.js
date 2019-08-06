3d10ebb370b779b873d05b0e0b356b8f5da30e55
walmir <walmir35@gmail.com>
2019-08-05 21:48:41 -0300

    segundo commit - axios.get - api github users - update

--- tindev/src/App.js
+++ tindev/src/App.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 function App() {
 
   const [name, setName] = useState('');
+  const [login, setLogin] = useState('');
+  const [location, setLocation] = useState('');
+  const [avatar, setAvatar] = useState('');
 
   useEffect(() => {
     axios.get('https://api.github.com/users/walmir35')
@@ -12,12 +15,18 @@ function App() {
       // handle success
       console.log(response.data);
       setName(response.data.name);
+      setLogin(response.data.login);
+      setLocation(response.data.location);
+      setAvatar(response.data.avatar_url);
     });
   });
 
   return (
-    <div className="App">
-      <h1>API GITHUB USER {name}</h1>
+    <div className='App'>
+      <h3>User {name} - {login} - {location}</h3><br />
+      <img alt='' width='260' height='260' 
+      className='avatar width-full height-full avatar-before-user-status' 
+      src={avatar} />
     </div>
   );
 }
